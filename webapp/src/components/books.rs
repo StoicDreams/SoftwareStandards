5a01c0969652543b4fab0d30a443918282927e2e
Erik Gassler <erik@gassler.us>
2024-01-12 06:03:12 -0600

    fix amazon book links

--- webapp/src/components/books.rs
+++ webapp/src/components/books.rs
@@ -12,27 +12,27 @@ pub(crate) fn case_books() -> Html {
         <Paper class={format!("{} gap-1", CLASSES_SIDE_BY_SIDE)}>
             <Paper class="d-flex flex-column justify-center">
                 <Quote color={Theme::Success} elevation={ELEVATION_STANDARD}>
-                    {r#"Get your copy of my book "CASE: Continous Agile Software Engineering" for more information about Continuous Agile Software Engineering and how to integrate it into your teams and workflows."#}
+                    {r#"
+                    Get your copy of my book "CASE: Continous Agile Software Engineering" for more information about Continuous Agile Software Engineering and how to integrate it into your teams and workflows.
+
+                    Note: These links use an affiliate tracker that allow us to earn an extra commission when you make a purchase."#}
                 </Quote>
             </Paper>
             <Paper class="d-flex flex-row pa-2 gap-2 justify-center flex-wrap">
                 <Paper elevation={ELEVATION_STANDARD} class="pa-2">
-                    <Paper class="theme-secondary text-center pa-3 mb-2">{"Kindle"}</Paper>
-                    <HtmlContent html={r#"
-                    <iframe sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin" style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=erikgassler-20&language=en_US&marketplace=amazon&region=US&placement=B0C4FX1F1S&asins=B0C4FX1F1S&linkId=f90be5ed0aa2e0038a6cddb219a5487c&show_border=true&link_opens_in_new_window=true"></iframe>
-                    "#} />
+                    <Link target="_blank" href="https://amzn.to/48S30VU">
+                        <Paper class="theme-secondary text-center pa-3">{"Kindle"}</Paper>
+                    </Link>
                 </Paper>
                 <Paper elevation={ELEVATION_STANDARD} class="pa-2">
-                    <Paper class="theme-tertiary text-center pa-3 mb-2">{"Paperback"}</Paper>
-                    <HtmlContent html={r#"
-                    <iframe sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin" style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=erikgassler-20&language=en_US&marketplace=amazon&region=US&placement=B0C47LV1Y8&asins=B0C47LV1Y8&linkId=73a4b5d19008b9eac1c02ba85efcb29c&show_border=true&link_opens_in_new_window=true"></iframe>
-                    "#} />
+                    <Link target="_blank" href="https://amzn.to/3Sgpyu3">
+                        <Paper class="theme-tertiary text-center pa-3">{"Paperback"}</Paper>
+                    </Link>
                 </Paper>
                 <Paper elevation={ELEVATION_STANDARD} class="pa-2">
-                    <Paper class="theme-primary text-center pa-3 mb-2">{"Hardcover"}</Paper>
-                    <HtmlContent html={r#"
-                    <iframe sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin" style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=erikgassler-20&language=en_US&marketplace=amazon&region=US&placement=B0C47YGGYH&asins=B0C47YGGYH&linkId=ad750159f22ea299d1ed66f4b21538cc&show_border=true&link_opens_in_new_window=true"></iframe>
-                    "#} />
+                    <Link target="_blank" href="https://amzn.to/3vue8db">
+                        <Paper class="theme-primary text-center pa-3">{"Hardcover"}</Paper>
+                    </Link>
                 </Paper>
             </Paper>
         </Paper>
