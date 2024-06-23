44b85f3e78a28e1aab3aad2c702021394bff0846
Erik Gassler <erik@gassler.us>
2024-06-23 09:41:11 -0500

    use webui.define

--- webapp/wc/books.js
+++ webapp/wc/books.js
@@ -1,42 +1,15 @@
 /* Display CASE books */
 "use strict"
-{
-    function toCamel(property) {
-        return property.replace(/(-[A-Za-z0-9]{1})/g, a => { return a[1].toUpperCase(); });
-    }
-    class Books extends HTMLElement {
-        constructor() {
-            super();
-            const t = this;
-            if (t.parentNode && t.parentNode.nodeName === 'P') {
-                let p = t.parentNode;
-                t.parentNode.parentNode.insertBefore(t, t.parentNode);
-                if (p.innerHTML.trim() === '') {
-                    p.remove();
-                }
-            }
-        }
-        static get observedAttributes() {
-            return [];
-        }
-        attributeChangedCallback(property, oldValue, newValue) {
-            property = toCamel(property);
-            if (oldValue === newValue) return;
-            if (newValue === null || newValue === undefined) {
-                delete this[property];
-            } else {
-                this[property] = newValue;
-            }
-        }
-        connectedCallback() {
-            this.innerHTML = webui.applyAppDataToContent(`
+webui.define("app-books", {
+    connected: (t) => {
+        t.innerHTML = webui.applyAppDataToContent(`
 <webui-side-by-side>
 
 <webui-flex column justify="center">
 
 <webui-quote theme="success">
 
-Get your copy of my book "CASE: Continous Agile Software Engineering" for more information about Continuous Agile Software Engineering and how to integrate it into your teams and workflows.
+Get your copy of the book "CASE: Continous Agile Software Engineering" for more information about Continuous Agile Software Engineering and how to integrate it into your teams and workflows.
 
 Note: These links use an affiliate tracker that allow us to earn an extra commission when you make a purchase.
 
@@ -80,15 +53,11 @@ Hardcover
 
 </webui-flex>
 
-*Paid Links*
+*As an Amazon Associate I earn from qualifying purchases.*
 
 </webui-flex>
 
 </webui-side-by-side>
 `);
-
-        }
-        disconnectedCallback() { }
     }
-    customElements.define('app-books', Books);
-}
+});
