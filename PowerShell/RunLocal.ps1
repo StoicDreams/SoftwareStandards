67684d3afd9aa991130792c91986aa2c172719d0
Erik Gassler <erik@gassler.us>
2026-09-01 11:25:11 -0500

    script update

--- PowerShell/RunLocal.ps1
+++ PowerShell/RunLocal.ps1
@@ -1,15 +1,25 @@
 param(
-    [int]$Port = 8091
+    [int]$Port = 8091,
+    [switch]$clean,
+    [switch]$PreBuildOnly
 )
 
 Set-Location $PSScriptRoot
 Set-Location ../
 
-cargo clean
-if (!(Test-Path "./target")) {
-    New-Item -ItemType Directory -Name "target"
+$ProjectName = (Get-Item .).Name
+$env:CARGO_TARGET_DIR = "D:\CargoTargets\$ProjectName"
+# cargo install sccache
+$env:RUSTC_WRAPPER = "sccache"
+
+if ($clean) {
+    cargo clean
 }
 if (!(Test-Path "./target/wasm32-unknown-unknown")) {
     New-Item -ItemType Directory -Name "target/wasm32-unknown-unknown"
+}
+if ($PreBuildOnly) {
+    cargo build --target wasm32-unknown-unknown
+    return
 }
 trunk serve --config webapp/Trunk.toml --port $Port
